2cc5ddc3cd8c4fe4f2a28847a744ffa982b1ab43
Raghav Kukreti <raghav.kukreti@gmail.com>
2019-05-18 22:15:45 +0530

    Basic auth works; need to add persistence

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python3    
 import json
+from script import SPOTIFY_AUTH_URL, CLIENT_ID, CLIENT_SECRET
 from flask import Flask, request, redirect, g, render_template
 import requests
 from urllib.parse import quote
 
-
 PORT = 8081
 app = Flask(__name__)
-    
-@app.route('/')
-def home():
-    return render_template('index.html')
 
+auth_check = False
+SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+SPOTIFY_API_BASE_URL = "https://api.spotify.com"
+API_VERSION = "v1"
+SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
+@app.route('/')
+def home(auth_check=auth_check):
+    return render_template('index.html', isAuthorized=auth_check)
+
+@app.route('/auth/')
+def authenticate():
+    return redirect(SPOTIFY_AUTH_URL)
+
+@app.route('/callback/q')
+def callback():
+    
+    return render_template('dashboard.html', data=profile_data)
+    
 if __name__ == '__main__':
     app.run(debug=True, port=PORT)
 
--- script.py
+++ script.py
@@ -1,13 +1,42 @@
+from urllib.parse import quote
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import json
+import requests
+from urllib.parse import quote
 
 CLIENT_ID = "eaaaf83e6d3647548dcb9a8c3ffa1149"
 CLIENT_SECRET = "5d8a3852d8ec457ca10d6bfab06a6701"
+SCOPES = "user-read-currently-playing user-read-recently-played"
+QUOTE_SCOPES=quote(SCOPES.encode("utf-8"))
+
+SPOTIFY_AUTH_URL = 'https://accounts.spotify.com/authorize?response_type=code&client_id={}&scope={}&redirect_uri={}'.format(CLIENT_ID, QUOTE_SCOPES, "http://127.0.0.1:8081/callback/q")
 
 client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-results = sp.search(q='weezer', limit=20)
-for i, t in enumerate(results['tracks']['items']):
-    print(' ', i, t['name'])
+def authorize():
+    auth_token = request.args['code']
+    code_payload = {
+        "grant_type": "authorization_code",
+        "code": str(auth_token),
+        "redirect_uri": "http://127.0.0.1:8081/callback/q",
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+    }
+    post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload)
+
+    # Auth Step 5: Tokens are Returned to Application
+    response_data = json.loads(post_request.text)
+
+    access_token = response_data["access_token"]
+    refresh_token = response_data["refresh_token"]
+    token_type = response_data["token_type"]
+    expires_in = response_data["expires_in"]
+
+    # Auth Step 6: Use the access token to access Spotify API
+    authorization_header = {"Authorization": "Bearer {}".format(access_token)}
+    user_profile_api_endpoint = "{}/me".format(SPOTIFY_API_URL)
+    profile_response = requests.get(user_profile_api_endpoint, headers=authorization_header)
+    profile_data = json.loads(profile_response.text)
